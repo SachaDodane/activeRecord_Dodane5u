9ca3efa9b191719b7bf90dc239c23270807eb164
Titoufeur <titouan.letondal@gmail.com>
2023-11-21 15:52:58 +0100

    Personne

---
+++ JDBC_QDEV/src/Personne.java
@@ -0,0 +1,11 @@
+public class Personne {
+    private int id;
+    private String nom;
+    private String prenom;
+
+    public Personne(String nom, String prenom){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.id = -1;
+    }
+}
